c60d0b298377cd9ccc022e184ebb627edda15187
jisoo <88030238+codesooo@users.noreply.github.com>
2023-10-14 18:09:29 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,26 +1,27 @@
 import sys
-n = int(sys.stdin.readline())
 
-stack=[]
-for i in range(n):
-    command = sys.stdin.readline().split()
+stack = []
 
-    if command[0]=='push':
-        stack.append(command[1])
-    elif command[0]=='pop':
-        if len(stack)==0:
+
+N = int(input())
+for _ in range(N):
+    s = sys.stdin.readline().split()
+    if s[0] == 'push':
+        stack.append(s[1])
+    elif s[0] == 'pop':
+        if len(stack) == 0:
             print(-1)
         else:
             print(stack.pop())
-    elif command[0] == 'size':
+    elif s[0] == 'size':
         print(len(stack))
-    elif command[0] == 'empty':
-        if len(stack)==0:
+    elif s[0] == 'empty':
+        if len(stack) == 0:
             print(1)
         else:
             print(0)
-    elif command[0] == 'top':
-        if len(stack)==0:
+    elif s[0] == 'top':
+        if len(stack) == 0:
             print(-1)
         else:
-            print(stack[-1])
+            print(stack[-1]) 

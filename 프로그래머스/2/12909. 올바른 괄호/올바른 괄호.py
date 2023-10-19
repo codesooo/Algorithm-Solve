b7d7b8d1858703d2b34ce3ecb79e1a2a9571a691
jisoo <88030238+codesooo@users.noreply.github.com>
2023-10-19 18:44:47 +0900

    [level 2] Title: 올바른 괄호, Time: 11.28 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,17 @@
+def solution(s):
+    stack = []
+    for i in s:
+        if i == ')':
+            if len(stack)!=0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(i)    
+        else:
+            stack.append(i)
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+        
+                
+            

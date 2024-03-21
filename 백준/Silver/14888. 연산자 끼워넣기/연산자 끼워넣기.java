5344d4d6b1f70dba7a4aa628b730be6480060d37
jisoo <wlttnrla@gmail.com>
2024-03-21 16:21:20 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 132 ms, Memory: 15604 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -2,57 +2,61 @@ import java.io.*;
 import java.util.*;
 public class Main {
     static int N;
-    static int[] nums;
+    static int[] A;
     static int[] operators = new int[4];
-    static int maxAns =  Integer.MIN_VALUE;
-    static int minAns =  Integer.MAX_VALUE;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
+
         N = Integer.parseInt(st.nextToken());
-        nums = new int[N];
-
-
+        A = new int[N];
         st = new StringTokenizer(bf.readLine());
         for(int i=0; i<N; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(bf.readLine());
         for(int i=0; i<4; i++){
             operators[i] = Integer.parseInt(st.nextToken());
         }
-        Bactracking(nums[0],1);
-        System.out.println(maxAns);
-        System.out.println(minAns);
+
+        rec(A[0], 1);
+        System.out.println(max);
+        System.out.println(min);
     }
 
-    public static void Bactracking(int now, int idx){
+    public static void rec(int now, int idx){
         if(idx == N){
-            maxAns = Math.max(maxAns, now);
-            minAns = Math.min(minAns, now);
+            max = Math.max(now,max);
+            min = Math.min(now,min);
             return;
         }
 
-        if(operators[0]>0){
-            operators[0]--;
-            Bactracking(now + nums[idx], idx+1);
-            operators[0]++;
-        }
-        if(operators[1]>0){
-            operators[1]--;
-            Bactracking(now - nums[idx], idx+1);
-            operators[1]++;
-        }
-        if(operators[2]>0){
-            operators[2]--;
-            Bactracking(now * nums[idx], idx+1);
-            operators[2]++;
-        }
-        if(operators[3]>0){
-            operators[3]--;
-            Bactracking(now / nums[idx], idx+1);
-            operators[3]++;
+        for(int i=0; i<4; i++){
+
+            if(operators[i]>0){
+                operators[i]--;
+
+                switch(i){
+                    case 0:
+                        rec(now + A[idx], idx + 1);
+                        break;
+                    case 1:
+                        rec(now - A[idx], idx + 1);
+                        break;
+                    case 2:
+                        rec(now * A[idx], idx + 1);
+                        break;
+                    case 3:
+                        rec(now / A[idx], idx+1);
+                        break;
+                }
+                operators[i]++;
+            }
         }
     }
 }
+

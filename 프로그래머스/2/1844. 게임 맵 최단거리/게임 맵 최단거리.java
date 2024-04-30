0592e0eb87ad64fec8a50f582b8fe1fc58f352ad
jisoo <wlttnrla@gmail.com>
2024-04-30 14:35:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.17 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,48 +1,50 @@
 import java.util.*;
 class Solution {
-    
+    static int[][] distance;
+    static boolean[][] visited;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
-    
-    
+    static int maps_x, maps_y;
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
         int answer = 0;
-        answer = BFS(0,0,maps,n,m);
+        maps_x = maps.length;
+        maps_y = maps[0].length;
+        distance = new int[maps_x][maps_y];
+        visited = new boolean[maps_x][maps_y];
         
-        return answer;
+        
+        
+        BFS(0,0, maps);
+        
+        if(distance[maps_x-1][maps_y-1] == 0){
+            return -1;
+        } else {
+            return distance[maps_x-1][maps_y-1];
+        }
     }
-    public int BFS(int x, int y, int[][] maps, int n, int m){
-        boolean[][] visited = new boolean[n][m];
-        int[][] distance = new int[n][m];
+    private void BFS(int x, int y, int[][] maps){
         Queue<int[]> queue = new LinkedList<>();
-        visited[x][y] = true;
         distance[x][y] = 1;
-        queue.add(new int[] {x,y});
+        visited[x][y] = true;
+        
+        queue.offer(new int[] {x,y});
         while(!queue.isEmpty()){
             int[] now = queue.poll();
+            
             for(int p=0; p<4; p++){
                 int nx = now[0] + dx[p];
                 int ny = now[1] + dy[p];
+                
+                if(nx >= maps_x || ny >= maps_y || nx < 0 || ny < 0){
+                    continue;
+                }
+                if(!visited[nx][ny] && maps[nx][ny]==1){
+                    distance[nx][ny] = distance[now[0]][now[1]] + 1;
+                    visited[nx][ny] = true;
+                    queue.offer(new int[] {nx,ny});
+                }
+            }
             
-            if(nx<0 || ny<0 || nx>=n || ny>=m){
-                continue;
-            }
-            if(!visited[nx][ny] && maps[nx][ny] == 1){
-                distance[nx][ny] = distance[now[0]][now[1]] + 1;
-
-                visited[nx][ny] = true;
-                queue.offer(new int[] {nx,ny});
-            }
-        
-            }
-        }
-        
-        if(distance[n-1][m-1] == 0){
-            return -1;
-        } else {
-            return distance[n-1][m-1];
         }
         
         

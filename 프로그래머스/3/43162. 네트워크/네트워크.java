fd752c013bbae04b89971c43c3dd37d7aec6ab57
jisoo <wlttnrla@gmail.com>
2024-04-30 11:52:14 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,27 +1,23 @@
 import java.util.*;
+
 class Solution {
+    static boolean[] visited;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] visited = new boolean[n];
-        
+        int answer=0;   
+        visited = new boolean[n];
         for(int i=0; i<n; i++){
             if(!visited[i]){
-                DFS(i,n,visited,computers);
+                DFS(i, n, computers);
                 answer++;
             }
-            
         }
-        
         return answer;
     }
-    public void DFS(int i, int n, boolean[] visited, int[][] computers){
-        visited[i] = true;
-        for(int j=0; j<n; j++){
-            if(i==j){
-                continue;
-            }
-            if(!visited[j] && computers[i][j] == 1){
-                DFS(j,n,visited,computers);
+    private void DFS(int now, int n, int[][] computers){
+        visited[now] = true;
+        for(int i=0; i<n; i++){
+            if(!visited[i] && computers[now][i]==1){
+                DFS(i,n,computers);
             }
         }
     }

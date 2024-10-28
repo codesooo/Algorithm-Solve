abfbdbef2c7d9d856efd7332d9bb5752110070ae
jisoo <wlttnrla@gmail.com>
2024-10-28 22:44:27 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[][] d;
+    private static final int INF = 1000000000;
+    public static void main(String[] args) throws IOException {
+        input();
+
+        solution();
+
+        System.out.println(getAnswer());
+    }
+
+    private static int getAnswer() {
+        int minSum = Integer.MAX_VALUE;
+        int minNum = 0;
+        for (int i = 1; i <= N; i++) {
+            int sum = 0;
+            for (int j = 1; j <= N; j++) {
+                sum += d[i][j];
+            }
+            if (minSum > sum) {
+                minSum = sum;
+                minNum = i;
+            } else if (minSum == sum) {
+                minNum = Math.min(minNum, i);
+            }
+        }
+        return minNum;
+    }
+
+    private static void solution() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (d[i][j] > d[i][k] + d[k][j]) {
+                        d[i][j] = d[i][k] + d[k][j];
+                    }
+                }
+            }
+        }
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        d = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    continue;
+                }
+                d[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            d[A][B] = 1;
+            d[B][A] = 1;
+        }
+    }
+}

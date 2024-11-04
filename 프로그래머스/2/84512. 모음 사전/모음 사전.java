69f9cac638e3cc0af5a16cb05375b48504aaa129
jisoo <wlttnrla@gmail.com>
2024-11-04 09:18:06 +0900

    [level 2] Title: 모음 사전, Time: 18.41 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,8 @@
-import java.util.*;
 class Solution {
-    private static final char[] Characters = {'A', 'E', 'I', 'O', 'U'};
-    private static List<String> wordList = new ArrayList<>();
     public int solution(String word) {
         int answer = 0;
-        
-        makeWords("");
-        
-        answer = wordList.indexOf(word);
-        return answer;
-    }
+        System.out.println(word);
     
-    private void makeWords(String word){
-        wordList.add(word);
-        if (word.length() == 5){
-            return;
-        }
-        
-        for(char c : Characters){
-            makeWords(word+c);
-        }
+        return answer;
     }
 }

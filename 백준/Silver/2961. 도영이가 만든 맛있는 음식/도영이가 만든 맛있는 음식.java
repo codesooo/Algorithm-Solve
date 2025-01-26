3083fdc8691996073b32a44f192ed74c1f546f6d
jisoo <wlttnrla@gmail.com>
2025-01-27 08:51:54 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 768 ms, Memory: 174172 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,57 +1,66 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int N;
-    static int[] S; // 입력받은 신맛
-    static int[] B; // 입력받은 쓴맛
+    private static int N;
+    private static int[] S_arr;
+    private static int[] B_arr;
 
-    static ArrayList<Integer> S_arr;    // 생성한 신맛 조합 저장
-    static ArrayList<Integer> B_arr;    // 생성한 쓴맛 조합 저장
-    static boolean[] visited;
-    static int min = Integer.MAX_VALUE;
+    private static ArrayList<Integer> S_list;
+    private static ArrayList<Integer> B_list;
 
-    static int S_sum;
-    static int B_sum;
+    private static boolean[] visited;
+    private static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException{
-        S_arr = new ArrayList<>();
-        B_arr = new ArrayList<>();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+    private static int S_sum;
+    private static int B_sum;
 
-        N = Integer.parseInt(st.nextToken());
-        S = new int[N];
-        B = new int[N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        S_arr = new int[N];
+        B_arr = new int[N];
+
+        S_list = new ArrayList<>();
+        B_list = new ArrayList<>();
+
         visited = new boolean[N];
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(bf.readLine());
-            S[i] = Integer.parseInt(st.nextToken());
-            B[i] = Integer.parseInt(st.nextToken());
-        }
-        rec(0);
-        System.out.println(min);
-    }
-    public static void rec(int cnt){
 
-        if(cnt == N){
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            S_arr[i] = Integer.parseInt(st.nextToken());
+            B_arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dfs(0);
+        System.out.println(min);
+
+    }
+
+    private static void dfs(int cnt) {
+        if (cnt == N) {
             return;
         }
-
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             S_sum = 1;
             B_sum = 0;
-            if(visited[i] == false){
-                S_arr.add(S[i]);
-                B_arr.add(B[i]);
+            if (visited[i] == false) {
+                S_list.add(S_arr[i]);
+                B_list.add(B_arr[i]);
                 visited[i] = true;
-                for(int j=0; j<S_arr.size(); j++){
-                    S_sum *= S_arr.get(j);
-                    B_sum += B_arr.get(j);
+
+                for (int j = 0; j < S_list.size(); j++) {
+                    S_sum *= S_list.get(j);
+                    B_sum += B_list.get(j);
                 }
+
                 min = Math.min(min, Math.abs(S_sum - B_sum));
-                rec(cnt+1);
-                S_arr.remove(S_arr.size()-1);
-                B_arr.remove(B_arr.size()-1);
+                dfs(cnt + 1);
+                S_list.remove(S_list.size() - 1);
+                B_list.remove(B_list.size() - 1);
                 visited[i] = false;
             }
         }
